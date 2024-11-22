89ab5a9aacaaa846dda919d7cbd6b8222cba57cb
aehrlich1 <anatol.ehrlich@gmail.com>
2024-11-22 14:43:21 +0100

    Implement calculation of S_IR and include rectangles in RPSMap showing SH and AC regions.

--- src/analysis/repra.py
+++ src/analysis/repra.py
@@ -1,9 +1,12 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 from scipy.spatial.distance import pdist
 from torch_geometric.datasets import MoleculeNet
 from torch_geometric.loader import DataLoader
+
+from src.models.model import ExtendedConnectivityFingerprintModel
 
 
 class Repra:
@@ -24,23 +27,26 @@ class Repra:
         self.dataloader = None
         self.embeddings = None
         self.properties = None
+        self.pairwise_embeddings_distances = None
         self.pairwise_embeddings_similarity = None
         self.pairwise_properties_similarity = None
         self.thresholds = None
         self.fingerprints = None
+        self.c1_c4_counts = None
+        self.c1_c4_ecfp_counts = None
 
         self.initialize_dataset(datasets[0])
         self.initialize_dataloader()
         self.initialize_properties()
-        self.calculate_embeddings()
-        self.calculate_pairwise_similarity()
-        self.calculate_pairwise_property_similarity()
-        self.calculate_thresholds()
-
+        self.initialize_embeddings()
+        self.initialize_pairwise_embeddings_distances()
+        self.initialize_pairwise_embeddings_similarity()
+        self.initialize_pairwise_property_similarity()
+        self.initialize_thresholds()
+        self.initialize_c1_c4_counts()
+        self.initialize_c1_c4_ecfp_counts()
+        self.calculate_improvement_rate()
         self.plot_rps_map()
-        # self.get_ac_and_sh_count()
-        # self.calculate_fingerprints()
-        # self.calculate_improvement_rate()
 
     def initialize_dataset(self, name) -> None:
         self.dataset = MoleculeNet(root=self.data_dir, name=name)
@@ -50,32 +56,47 @@ class Repra:
             self.dataset, batch_size=len(self.dataset), num_workers=0
         )
 
-    def initialize_properties(self):
+    def initialize_properties(self) -> None:
         self.properties = self.dataset.y.detach().numpy().reshape(-1, 1)
 
-    def calculate_embeddings(self):
-        embeddings = self.model(next(iter(self.dataloader)))
-        if torch.is_tensor(embeddings):
-            self.embeddings = embeddings.detach().numpy()
-        self.embeddings = embeddings
+    def initialize_embeddings(self) -> None:
+        self.embeddings = self.calculate_embeddings(self.model)
 
-    def calculate_pairwise_similarity(self):
+    def initialize_pairwise_embeddings_distances(self) -> None:
+        self.pairwise_embeddings_distances = self.calculate_pairwise_distances(self.embeddings)
+
+    def initialize_pairwise_embeddings_similarity(self) -> None:
+        self.pairwise_embeddings_similarity = self.calculate_pairwise_similarity(
+            self.embeddings
+        )
+
+    def calculate_embeddings(self, model):
+        embeddings = model(next(iter(self.dataloader)))
+        if torch.is_tensor(embeddings):
+            embeddings = embeddings.detach().numpy()
+
+        return embeddings
+
+    def calculate_pairwise_distances(self, embeddings):
+        return self.min_max_eud(embeddings)
+
+    def calculate_pairwise_similarity(self, embeddings):
         # cos_sim = 1 - cos_dist
         # returns a sparse vector with all pairwise distances
-        self.pairwise_embeddings_similarity = 1 - self.min_max_eud(self.embeddings)
+        return 1 - self.min_max_eud(embeddings)
 
-    def calculate_pairwise_property_similarity(self):
+    def initialize_pairwise_property_similarity(self) -> None:
         self.pairwise_properties_similarity = self.min_max_eud(self.properties)
 
-    def calculate_thresholds(self):
-        median = np.median(self.pairwise_embeddings_similarity)
-        d_near_mask = self.pairwise_embeddings_similarity < median
+    def initialize_thresholds(self) -> None:
+        median = np.median(self.pairwise_embeddings_distances)
+        d_near_mask = self.pairwise_embeddings_distances < median
         d_far_mask = ~d_near_mask
 
-        delta_1 = np.mean(self.pairwise_embeddings_similarity[d_near_mask])
+        delta_1 = np.mean(self.pairwise_embeddings_distances[d_near_mask])
         epsilon_1 = np.mean(self.pairwise_properties_similarity[d_near_mask])
 
-        delta_2 = np.mean(self.pairwise_embeddings_similarity[d_far_mask])
+        delta_2 = np.mean(self.pairwise_embeddings_distances[d_far_mask])
         epsilon_2 = np.mean(self.pairwise_properties_similarity[d_far_mask])
 
         self.thresholds = {
@@ -83,6 +104,15 @@ class Repra:
             "epsilon_1": epsilon_1,
             "delta_2": delta_2,
             "epsilon_2": epsilon_2,
+        }
+
+        print(self.thresholds)
+
+    def initialize_c1_c4_counts(self) -> None:
+        c1, c4 = self.calculate_c1_and_c4_count(self.pairwise_embeddings_similarity)
+        self.c1_c4_counts = {
+            "c1": c1,
+            "c4": c4,
         }
 
     @staticmethod
@@ -95,31 +125,76 @@ class Repra:
             max_pairwise_dist - min_pairwise_dist
         )
 
-    def calculate_fingerprint_embeddings(self):
-        pass
+    def initialize_c1_c4_ecfp_counts(self) -> None:
+        ecfp_model = ExtendedConnectivityFingerprintModel()
+        ecfp_embeddings = self.calculate_embeddings(ecfp_model)
+        ecfp_pairwise_embedding_similarities = self.calculate_pairwise_similarity(
+            ecfp_embeddings
+        )
+        c1_ecfp, c4_ecfp = self.calculate_c1_and_c4_count(
+            ecfp_pairwise_embedding_similarities
+        )
+        self.c1_c4_ecfp_counts = {
+            "c1": c1_ecfp,
+            "c4": c4_ecfp,
+        }
 
-    def get_ac_and_sh_count(self):
-        # SH
-        a = self.pairwise_embeddings_similarity < 1 - self.thresholds["delta_2"]
-        if (self.pairwise_embeddings_similarity < 1 - self.thresholds["delta_1"]) & (
-            self.pairwise_properties_similarity < self.properties["epsilon_1"]
-        ):
-            pass
+    def calculate_c1_and_c4_count(self, pairwise_embeddings_similarity):
+        c1 = np.sum(
+            (pairwise_embeddings_similarity < 1 - self.thresholds["delta_2"])
+            & (self.pairwise_properties_similarity < self.thresholds["epsilon_2"])
+        )
+
+        c4 = np.sum(
+            (pairwise_embeddings_similarity > 1 - self.thresholds["delta_1"])
+            & (self.pairwise_properties_similarity > self.thresholds["epsilon_1"])
+        )
+
+        return c1, c4
 
     def calculate_average_deviation(self):
         pass
 
     def calculate_improvement_rate(self):
-        pass
+        improvement_rate = (self.c1_c4_counts["c1"] / self.c1_c4_ecfp_counts["c1"]) + (
+            self.c1_c4_counts["c4"] / self.c1_c4_ecfp_counts["c4"]
+        )
+        print(f"Improvement rate: {improvement_rate}")
+        print("Hi")
 
     def plot_rps_map(self):
         x = self.pairwise_embeddings_similarity
         y = self.pairwise_properties_similarity
 
-        fig = plt.figure(figsize=(8, 6), dpi=200, facecolor="w", edgecolor="k")
-        axes = fig.add_axes([0.13, 0.13, 0.8, 0.8])
-        axes.scatter(x, y, marker="o", s=12, color="salmon", edgecolors="grey")
-        plt.show()
+        fig, ax = plt.subplots(figsize=(8, 6), dpi=200, facecolor="w", edgecolor="k")
+        ax.set_position([0.13, 0.13, 0.8, 0.8])
+        ax.scatter(x, y, marker="o", s=12, color="salmon", edgecolors="grey")
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
 
-    def _min_max_eud(self):
-        pass
+        rectangle_sh = Rectangle(
+            (0, 0),
+            height=self.thresholds["epsilon_2"],
+            width=1 - self.thresholds["delta_2"],
+            linewidth=1,
+            edgecolor="grey",
+            facecolor="grey",
+            alpha=0.4,
+        )
+
+        rectangle_ac = Rectangle(
+            (1 - self.thresholds["delta_1"], self.thresholds["epsilon_1"]),
+            height=1,
+            width=1,
+            linewidth=1,
+            edgecolor="grey",
+            facecolor="grey",
+            alpha=0.4,
+        )
+
+        ax.add_patch(rectangle_sh)
+        ax.add_patch(rectangle_ac)
+
+        plt.xlabel("Representational Similarity")
+        plt.ylabel("Property Distance")
+        plt.show()
--- tests/test_repra.py
+++ tests/test_repra.py
@@ -8,11 +8,18 @@ def test_repra():
     pretrain.load_pretrained_model("dauntless-planet-7")
     pretrain.model.eval()
 
-    # 2. perform repra analysis
     repra = Repra(pretrain.model, ["ESOL"], "../data/molecule_net")
-    assert True
+    assert repra
 
 def test_repra_ecfp():
     model = ExtendedConnectivityFingerprintModel()
     repra = Repra(model, ["ESOL"], "../data/molecule_net")
-    assert True
+    assert repra
+
+def test_repra_random_model():
+    pretrain = Pretrain()
+    pretrain.load_random_model(dim_h=128, dropout=0.1)
+    pretrain.model.eval()
+
+    repra = Repra(pretrain.model, ["ESOL"], "../data/molecule_net")
+    assert repra
