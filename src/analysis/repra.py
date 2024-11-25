166d0bfba8256ec5b08e0e874df1c5c83af071c3
aehrlich1 <anatol.ehrlich@gmail.com>
2024-11-25 09:59:21 +0100

    Merge repra_v2 into repra

--- src/analysis/repra.py
+++ src/analysis/repra.py
@@ -11,112 +11,127 @@ from src.models.model import ExtendedConnectivityFingerprintModel
 
 class Repra:
     def __init__(
-        self,
-        model,
-        datasets: list,
-        data_dir,
-        task_type="regression",
-        metric="min_max_eud",
+            self,
+            model,
+            dataset_name: str,
+            data_dir: str,
+            task_type: str = "regression",
+            metric: str = "min_max_eud",
     ):
         self.model = model
-        self.datasets = datasets
+        self.dataset_name = dataset_name
         self.data_dir = data_dir
         self.task_type = task_type
         self.metric = metric
-        self.dataset = None
-        self.dataloader = None
-        self.embeddings = None
-        self.properties = None
-        self.pairwise_embeddings_distances = None
-        self.pairwise_embeddings_similarity = None
-        self.pairwise_properties_similarity = None
-        self.thresholds = None
-        self.fingerprints = None
-        self.c1_c4_counts = None
-        self.c1_c4_ecfp_counts = None
 
-        self.initialize_dataset(datasets[0])
-        self.initialize_dataloader()
-        self.initialize_properties()
-        self.initialize_embeddings()
-        self.initialize_pairwise_embeddings_distances()
-        self.initialize_pairwise_embeddings_similarity()
-        self.initialize_pairwise_property_similarity()
-        self.initialize_thresholds()
-        self.initialize_c1_c4_counts()
-        self.initialize_c1_c4_ecfp_counts()
-        self.calculate_improvement_rate()
-        self.calculate_average_deviation()
-        self.plot_rps_map()
-
-    def initialize_dataset(self, name) -> None:
-        self.dataset = MoleculeNet(root=self.data_dir, name=name)
-
-    def initialize_dataloader(self) -> None:
-        self.dataloader = DataLoader(
-            self.dataset, batch_size=len(self.dataset), num_workers=0
+    def analyze(self):
+        dataset = self.get_dataset()
+        dataloader = self.get_dataloader(dataset)
+        embeddings = self.calculate_embeddings(self.model, dataloader)
+        pairwise_embeddings_distances = self.calculate_pairwise_distances(embeddings)
+        properties = self.get_properties(dataset)
+        pairwise_properties_distances = self.calculate_pairwise_distances(properties)
+        thresholds = self.calculate_thresholds(
+            pairwise_embeddings_distances=pairwise_embeddings_distances,
+            pairwise_properties_distances=pairwise_properties_distances,
+        )
+        c1_ptm, c4_ptm = self.calculate_c1_and_c4_count(
+            pairwise_embeddings_distances=pairwise_embeddings_distances,
+            pairwise_properties_distances=pairwise_properties_distances,
+            thresholds=thresholds,
         )
 
-    def initialize_properties(self) -> None:
-        self.properties = self.dataset.y.detach().numpy().reshape(-1, 1)
-
-    def initialize_embeddings(self) -> None:
-        self.embeddings = self.calculate_embeddings(self.model)
-
-    def initialize_pairwise_embeddings_distances(self) -> None:
-        self.pairwise_embeddings_distances = self.calculate_pairwise_distances(
-            self.embeddings
+        model_fp = self.get_model_fp()
+        embeddings_fp = self.calculate_embeddings(model_fp, dataloader)
+        pairwise_embeddings_distances_fp = self.calculate_pairwise_distances(
+            embeddings_fp
+        )
+        c1_fp, c4_fp = self.calculate_c1_and_c4_count(
+            pairwise_embeddings_distances=pairwise_embeddings_distances_fp,
+            pairwise_properties_distances=pairwise_properties_distances,
+            thresholds=thresholds,
         )
 
-    def initialize_pairwise_embeddings_similarity(self) -> None:
-        self.pairwise_embeddings_similarity = self.calculate_pairwise_similarity(
-            self.embeddings
+        improvement_rate = self.calculate_improvement_rate(c1_ptm, c4_ptm, c1_fp, c4_fp)
+        average_deviation = self.calculate_average_deviation(
+            pairwise_embeddings_distances=pairwise_embeddings_distances,
+            pairwise_properties_distances=pairwise_properties_distances,
+            thresholds=thresholds,
         )
 
-    def calculate_embeddings(self, model):
-        embeddings = model(next(iter(self.dataloader)))
+        self.plot_rps_map(
+            pairwise_embeddings_distances=pairwise_embeddings_distances,
+            pairwise_properties_distances=pairwise_properties_distances,
+            thresholds=thresholds,
+        )
+
+        print(f"Improvement rate: {improvement_rate}")
+        return improvement_rate, average_deviation
+
+    def get_dataset(self):
+        return MoleculeNet(root=self.data_dir, name=self.dataset_name)
+
+    @staticmethod
+    def get_model_fp():
+        return ExtendedConnectivityFingerprintModel()
+
+    @staticmethod
+    def get_dataloader(dataset):
+        return DataLoader(dataset, batch_size=len(dataset), num_workers=0)
+
+    @staticmethod
+    def calculate_embeddings(model, dataloader):
+        embeddings = model(next(iter(dataloader)))
         if torch.is_tensor(embeddings):
             embeddings = embeddings.detach().numpy()
-
         return embeddings
 
-    def calculate_pairwise_distances(self, embeddings):
-        return self.min_max_eud(embeddings)
+    @staticmethod
+    def calculate_pairwise_distances(embeddings):
+        return Repra.min_max_eud(embeddings)
 
-    def calculate_pairwise_similarity(self, embeddings):
-        # cos_sim = 1 - cos_dist
-        # returns a sparse vector with all pairwise distances
-        return 1 - self.min_max_eud(embeddings)
+    @staticmethod
+    def calculate_c1_and_c4_count(
+            pairwise_embeddings_distances, pairwise_properties_distances, thresholds
+    ):
+        pairwise_embeddings_similarities = 1 - pairwise_embeddings_distances
 
-    def initialize_pairwise_property_similarity(self) -> None:
-        self.pairwise_properties_similarity = self.min_max_eud(self.properties)
+        c1 = np.sum(
+            (pairwise_embeddings_similarities < 1 - thresholds["delta_2"])
+            & (pairwise_properties_distances < thresholds["epsilon_2"])
+        )
 
-    def initialize_thresholds(self) -> None:
-        median = np.median(self.pairwise_embeddings_distances)
-        d_near_mask = self.pairwise_embeddings_distances < median
+        c4 = np.sum(
+            (pairwise_embeddings_similarities > 1 - thresholds["delta_1"])
+            & (pairwise_properties_distances > thresholds["epsilon_1"])
+        )
+
+        return c1, c4
+
+    @staticmethod
+    def calculate_thresholds(
+            pairwise_embeddings_distances, pairwise_properties_distances
+    ):
+        median = np.median(pairwise_embeddings_distances)
+        d_near_mask = pairwise_embeddings_distances < median
         d_far_mask = ~d_near_mask
 
-        delta_1 = np.mean(self.pairwise_embeddings_distances[d_near_mask])
-        epsilon_1 = np.mean(self.pairwise_properties_similarity[d_near_mask])
+        delta_1 = np.mean(pairwise_embeddings_distances[d_near_mask])
+        epsilon_1 = np.mean(pairwise_properties_distances[d_near_mask])
 
-        delta_2 = np.mean(self.pairwise_embeddings_distances[d_far_mask])
-        epsilon_2 = np.mean(self.pairwise_properties_similarity[d_far_mask])
+        delta_2 = np.mean(pairwise_embeddings_distances[d_far_mask])
+        epsilon_2 = np.mean(pairwise_properties_distances[d_far_mask])
 
-        self.thresholds = {
+        return {
             "delta_1": delta_1,
             "epsilon_1": epsilon_1,
             "delta_2": delta_2,
             "epsilon_2": epsilon_2,
         }
 
-        print(self.thresholds)
-
-    def initialize_c1_c4_counts(self) -> None:
-        c1, c4 = self.calculate_c1_and_c4_count(self.pairwise_embeddings_similarity)
-        self.c1_c4_counts = {
-            "c1": c1,
-            "c4": c4,
-        }
+    @staticmethod
+    def get_properties(dataset):
+        return dataset.y.detach().numpy().reshape(-1, 1)
 
     @staticmethod
     def min_max_eud(X):
@@ -125,54 +140,77 @@ class Repra:
         min_pairwise_dist = min(pairwise_dist)
         max_pairwise_dist = max(pairwise_dist)
         return (pairwise_dist - min_pairwise_dist) / (
-            max_pairwise_dist - min_pairwise_dist
+                max_pairwise_dist - min_pairwise_dist
         )
 
-    def initialize_c1_c4_ecfp_counts(self) -> None:
-        ecfp_model = ExtendedConnectivityFingerprintModel()
-        ecfp_embeddings = self.calculate_embeddings(ecfp_model)
-        ecfp_pairwise_embedding_similarities = self.calculate_pairwise_similarity(
-            ecfp_embeddings
-        )
-        c1_ecfp, c4_ecfp = self.calculate_c1_and_c4_count(
-            ecfp_pairwise_embedding_similarities
-        )
-        self.c1_c4_ecfp_counts = {
-            "c1": c1_ecfp,
-            "c4": c4_ecfp,
-        }
+    @staticmethod
+    def calculate_improvement_rate(c1_ptm, c4_ptm, c1_fp, c4_fp):
+        improvement_rate = (c1_ptm / c1_fp) + (c4_ptm / c4_fp)
+        return improvement_rate
 
-    def calculate_c1_and_c4_count(self, pairwise_embeddings_similarity):
-        c1 = np.sum(
-            (pairwise_embeddings_similarity < 1 - self.thresholds["delta_2"])
-            & (self.pairwise_properties_similarity < self.thresholds["epsilon_2"])
+    @staticmethod
+    def calculate_average_deviation(
+            pairwise_embeddings_distances, pairwise_properties_distances, thresholds
+    ):
+        (
+            pairwise_embeddings_similarities_r1,
+            pairwise_properties_distances_r1,
+            pairwise_embeddings_similarities_r4,
+            pairwise_properties_distances_r4,
+        ) = Repra.get_r1_r4_points(
+            pairwise_embeddings_distances, pairwise_properties_distances, thresholds
         )
 
-        c4 = np.sum(
-            (pairwise_embeddings_similarity > 1 - self.thresholds["delta_1"])
-            & (self.pairwise_properties_similarity > self.thresholds["epsilon_1"])
+        abs_prop_dist_diff_r1 = np.abs(
+            pairwise_properties_distances_r1 - thresholds["epsilon_2"]
+        )
+        abs_sim_dist_diff_r1 = np.abs(
+            pairwise_embeddings_similarities_r1 - (1 - thresholds["delta_2"])
         )
 
-        return c1, c4
-
-    def calculate_improvement_rate(self):
-        improvement_rate = (self.c1_c4_counts["c1"] / self.c1_c4_ecfp_counts["c1"]) + (
-            self.c1_c4_counts["c4"] / self.c1_c4_ecfp_counts["c4"]
+        abs_prop_dist_diff_r4 = np.abs(
+            pairwise_properties_distances_r4 - thresholds["epsilon_1"]
         )
-        print(f"Improvement rate: {improvement_rate}")
+        abs_sim_dist_diff_r4 = np.abs(
+            pairwise_embeddings_similarities_r4 - (1 - thresholds["delta_1"])
+        )
 
-    def calculate_average_deviation(self):
-        abs_prop_dist_diff = np.abs(self.pairwise_properties_similarity - self.thresholds["epsilon_2"])
-        abs_sim_dist_diff = np.abs(self.pairwise_embeddings_similarity - (1 - self.thresholds["delta_2"]))
+        m_r1 = np.sum(np.minimum(abs_prop_dist_diff_r1, abs_sim_dist_diff_r1))
+        m_r4 = np.sum(np.minimum(abs_prop_dist_diff_r4, abs_sim_dist_diff_r4))
+        s_ad = (m_r1 + m_r4) / (len(pairwise_embeddings_distances))
 
-        m = np.sum(np.minimum(abs_prop_dist_diff, abs_sim_dist_diff))
-        s_ad = m / (len(abs_prop_dist_diff))
-        print(s_ad)
-        pass
+        return s_ad
 
-    def plot_rps_map(self):
-        x = self.pairwise_embeddings_similarity
-        y = self.pairwise_properties_similarity
+    @staticmethod
+    def get_r1_r4_points(
+            pairwise_embeddings_distances, pairwise_properties_distances, thresholds
+    ):
+        pairwise_embeddings_similarities = 1 - pairwise_embeddings_distances
+
+        r1_mask = (pairwise_embeddings_similarities < 1 - thresholds["delta_2"]) & (
+                pairwise_properties_distances < thresholds["epsilon_2"]
+        )
+
+        r4_mask = (pairwise_embeddings_similarities > 1 - thresholds["delta_1"]) & (
+                pairwise_properties_distances > thresholds["epsilon_1"]
+        )
+
+        return (
+            pairwise_embeddings_similarities[r1_mask],
+            pairwise_properties_distances[r1_mask],
+            pairwise_embeddings_similarities[r4_mask],
+            pairwise_properties_distances[r4_mask],
+        )
+
+    @staticmethod
+    def plot_rps_map(
+            pairwise_embeddings_distances,
+            pairwise_properties_distances,
+            thresholds,
+    ):
+        pairwise_embeddings_similarities = 1 - pairwise_embeddings_distances
+        x = pairwise_embeddings_similarities
+        y = pairwise_properties_distances
 
         fig, ax = plt.subplots(figsize=(8, 6), dpi=200, facecolor="w", edgecolor="k")
         ax.set_position([0.13, 0.13, 0.8, 0.8])
@@ -182,8 +220,8 @@ class Repra:
 
         rectangle_sh = Rectangle(
             (0, 0),
-            height=self.thresholds["epsilon_2"],
-            width=1 - self.thresholds["delta_2"],
+            height=thresholds["epsilon_2"],
+            width=1 - thresholds["delta_2"],
             linewidth=1,
             edgecolor="grey",
             facecolor="grey",
@@ -191,7 +229,7 @@ class Repra:
         )
 
         rectangle_ac = Rectangle(
-            (1 - self.thresholds["delta_1"], self.thresholds["epsilon_1"]),
+            (1 - thresholds["delta_1"], thresholds["epsilon_1"]),
             height=1,
             width=1,
             linewidth=1,
--- tests/test_repra.py
+++ tests/test_repra.py
@@ -1,22 +1,16 @@
 from src.analysis.repra import Repra
-from src.analysis.repra_v2 import RepraV2
 from src.models.model import ExtendedConnectivityFingerprintModel
 from src.models.pretrain import Pretrain
 
 
-def test_repra():
-    pretrain = Pretrain(data_dir="../data")
-    pretrain.load_pretrained_model("dauntless-planet-7")
-    pretrain.model.eval()
-
-    repra = Repra(pretrain.model, ["ESOL"], "../data/molecule_net")
-    assert repra
-
-
-def test_repra_ecfp():
+def test_repra_fp():
     model = ExtendedConnectivityFingerprintModel()
-    repra = Repra(model, ["ESOL"], "../data/molecule_net")
-    assert repra
+    repra = Repra(model, "ESOL", "../data/molecule_net")
+    improvement_rate, average_deviation = repra.analyze()
+    print(
+        f"improvement rate: {improvement_rate}, average deviation: {average_deviation}"
+    )
+    assert improvement_rate == 2.0
 
 
 def test_repra_random_model():
@@ -24,32 +18,22 @@ def test_repra_random_model():
     pretrain.load_random_model(dim_h=128, dropout=0.1)
     pretrain.model.eval()
 
-    repra = Repra(pretrain.model, ["ESOL"], "../data/molecule_net")
+    repra = Repra(pretrain.model, "ESOL", "../data/molecule_net")
+    improvement_rate, average_deviation = repra.analyze()
+    print(
+        f"improvement rate: {improvement_rate}, average deviation: {average_deviation}"
+    )
     assert repra
 
-def test_repra_v2():
+
+def test_repra_pretrained():
     pretrain = Pretrain(data_dir="../data")
     pretrain.load_pretrained_model("dauntless-planet-7")
     pretrain.model.eval()
 
-    repra_v2 = RepraV2(pretrain.model, "ESOL", "../data/molecule_net")
-    improvement_rate, average_deviation = repra_v2.analyze()
-    print(f"improvement rate: {improvement_rate}, average deviation: {average_deviation}")
-    assert repra_v2
-
-def test_repra_v2_ecfp():
-    model = ExtendedConnectivityFingerprintModel()
-    repra_v2 = RepraV2(model, "ESOL", "../data/molecule_net")
-    improvement_rate, average_deviation = repra_v2.analyze()
-    assert improvement_rate == 2.0
-
-
-def test_repra_v2_random():
-    pretrain = Pretrain()
-    pretrain.load_random_model(dim_h=128, dropout=0.1)
-    pretrain.model.eval()
-
-    repra_v2 = RepraV2(pretrain.model, "ESOL", "../data/molecule_net")
-    improvement_rate, average_deviation = repra_v2.analyze()
-    print(f"improvement rate: {improvement_rate}, average deviation: {average_deviation}")
-    assert repra_v2
+    repra = Repra(pretrain.model, "ESOL", "../data/molecule_net")
+    improvement_rate, average_deviation = repra.analyze()
+    print(
+        f"improvement rate: {improvement_rate}, average deviation: {average_deviation}"
+    )
+    assert repra
