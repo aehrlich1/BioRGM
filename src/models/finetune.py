a62d05b5c20cab91f23c92404cc035e26e0e860a
aehrlich1 <anatol.ehrlich@gmail.com>
2024-12-05 16:50:53 +0100

    implement wandb logging

--- src/models/finetune.py
+++ src/models/finetune.py
@@ -1,4 +1,5 @@
 import torch
+import wandb
 from torch.optim import Adam
 from sklearn.metrics import roc_auc_score
 from torch.nn import BCELoss
@@ -16,47 +17,63 @@ def finetune(pretrain_model, dataset: str, data_dir: str) -> None:
     3. load pre-trained model
     4. perform fine-tuning on dataset
     """
+    wandb.init(
+        project="BioRGM_finetune",
+    )
 
     dataset = _get_dataset(dataset, data_dir)
     train_dataloader, test_dataloader = _get_dataloaders(dataset)
     finetune_model = _initialize_finetune_model(pretrain_model)
 
-    _test_loop(finetune_model, test_dataloader)
+    _test_loop(finetune_model, test_dataloader, epoch=0)
     for epoch in range(200):
         print(f"\nEpoch {epoch+1}\n-------------------------------\n")
-        _train_loop(finetune_model, train_dataloader)
-        _test_loop(finetune_model, test_dataloader)
+        _train_loop(finetune_model, train_dataloader, epoch)
+        _test_loop(finetune_model, test_dataloader, epoch)
 
 
-def _train_loop(model, dataloader):
+def _train_loop(model, dataloader, epoch):
     model.train()
     loss_fn = BCELoss()
     optimizer = Adam(model.parameters(), lr=0.00001)
+    batch_loss = 0
 
     for data in dataloader:
         out = model(data)
         loss = loss_fn(out, data.y)
+        batch_loss += loss.item()
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+    average_loss = batch_loss / len(dataloader)
+    wandb.log({"training loss": average_loss}, step=epoch)
 
-def _test_loop(model, dataloader):
+
+def _test_loop(model, dataloader, epoch):
     model.eval()
-
+    loss_fn = BCELoss()
+    batch_loss = 0
     y_true, y_pred = [], []
 
     with torch.no_grad():
         for data in dataloader:
             out = model(data)
+            loss = loss_fn(out, data.y)
+            batch_loss += loss.item()
             y_true.append(data.y.view(-1))
             y_pred.append(out.view(-1))
 
     y_true = torch.cat(y_true)
     y_pred = torch.cat(y_pred)
-
+    average_loss = batch_loss / len(dataloader)
     roc_auc = roc_auc_score(y_true, y_pred)
+
+    wandb.log({"test loss": average_loss}, step=epoch)
+    wandb.log({"ROC AUC": roc_auc}, step=epoch)
+
     print(f"ROC AUC: {roc_auc}")
+    return average_loss, roc_auc
 
 
 def _get_dataset(dataset: str, data_dir: str) -> DataLoader:
